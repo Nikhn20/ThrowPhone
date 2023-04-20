a32b32ba16d68edee078c1064f3077f5732d7eda
Nikhn20 <112695082+Nikhn20@users.noreply.github.com>
2023-04-20 11:23:38 +0200

    Unden Test

--- app/src/main/java/com/example/throwphone/MainActivity.java
+++ app/src/main/java/com/example/throwphone/MainActivity.java
@@ -2,32 +2,56 @@ package com.example.throwphone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.throwphone.SensorManagerJava;
 
-    //Fun fact cold showers are cold
-    //Fun fact
+public class MainActivity extends Activity implements SensorEventListener {
 
+
+    private TextView accText;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public final void onCreate (Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button statButton = (Button) findViewById(R.id.buttonStat);
+        SensorManager rSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        Sensor accSensor = rSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        rSensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
 
-        statButton.setOnClickListener(new View.OnClickListener(){
+        accText = findViewById(R.id.ScoreHere);
 
-            @Override public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, StatActivity.class));
-            }
+        accText.setText("working");
+    }
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        float accX = sensorEvent.values[0];
+        float accY = sensorEvent.values[1];
+        float accZ = sensorEvent.values[2];
+
+        double accMagnitude = Math.sqrt(accX * accX + accY * accY + accZ * accZ);
+
+        double dropAcc = accMagnitude - SensorManager.GRAVITY_EARTH;
+
+
+        if (dropAcc > 1) {
+            accText.setText("" + dropAcc + "");
         }
+    }
 
-        );
-
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
 
     }
+
 }
--- app/src/main/java/com/example/throwphone/SensorManagerJava.java
+++ app/src/main/java/com/example/throwphone/SensorManagerJava.java
@@ -9,38 +9,10 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import org.w3c.dom.Text;
 
-public class SensorManagerJava extends Activity implements SensorEventListener {
+public class SensorManagerJava extends AppCompatActivity {
 
-    private TextView accText;
-    @Override
-    public final void onCreate (Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        setContentView(R.layout.activity_stats);
-
-        SensorManager rSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor accSensor = rSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        rSensorManager.registerListener(this, accSensor, SensorManager.SENSOR_DELAY_NORMAL);
-
-        accText = findViewById(R.id.accTextView);
-    }
-    @Override
-    public void onSensorChanged(SensorEvent sensorEvent) {
-        float accX = sensorEvent.values[0];
-        float accY = sensorEvent.values[1];
-        float accZ = sensorEvent.values[2];
-
-        double accMagnitude = Math.sqrt(accX * accX + accY * accY + accZ * accZ);
-
-        double dropAcc = accMagnitude - SensorManager.GRAVITY_EARTH;
-
-accText.setText("" + dropAcc + "");
-    }
-
-    @Override
-    public void onAccuracyChanged(Sensor sensor, int i) {
-
-    }
 }
